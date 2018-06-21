1be367c29d20c2cd83ddcf95f73f4a04e6ac6fca
chandra1960 <38549511+chandra1960@users.noreply.github.com>
2018-06-21 00:40:32 -0500

    Add files via upload

---
+++ SpringMVCForm/src/com/spring/controller/RegisterController.java
@@ -0,0 +1,46 @@
+package com.spring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.model.User;
+
+@Controller
+@RequestMapping(value = "/register")
+public class RegisterController {
+
+@RequestMapping(method = RequestMethod.GET)
+public String viewRegistration(Map<String, Object> model) {
+    User userForm = new User();    
+    model.put("userForm", userForm);
+     
+    List<String> professionList = new ArrayList<>();
+    professionList.add("Software Developer");
+    professionList.add("Software Designer");
+    professionList.add("IT Manager");
+    model.put("professionList", professionList);
+     
+    return "Registration";
+}
+ 
+@RequestMapping(method = RequestMethod.POST)
+public String processRegistration(@ModelAttribute("userForm") User user,
+        Map<String, Object> model) {
+     
+
+    System.out.println("username: " + user.getUsername());
+    System.out.println("password: " + user.getPassword());
+    System.out.println("email: " + user.getEmail());
+    System.out.println("birth date: " + user.getBirthDate());
+    System.out.println("profession: " + user.getProfession());
+
+    return "RegistrationSuccess";
+}
+
+}
